cf9d2dd112ae46411b57ed82ad39b76630903449
dqian <davidqian1996@gmail.com>
2016-11-23 00:08:21 -0500

    still need a better explanation

--- assign_cookies.py
+++ assign_cookies.py
@@ -1,6 +1,7 @@
 # Problem: Assign Cookies
 # (https://leetcode.com/problems/assign-cookies/)
 
+# TODO: REDO EXPLANATION
 
 # NOTES:
 # -set of cookies with size ci
@@ -27,20 +28,46 @@
 # The second best match is where ci - 1 == gi (not optimal assignment
 # of cookie to kid since ci could be used on smaller greed kids)
 
-# ci - 2 == gi (even better cookie, could satisfy more matches)
+# ci - 2 == gi (this cookie could satisfy even more matches! too good
+# for it?)
 # ...until...
-# assign largest ci to a greed gi.
+# assign largest ci to a greed gi -- assign the best cookie possible
+# to this greed?
 
-# The best use of a cookie is the optimal match (ci == gi).
-# The second best use is a match where (ci - 1 == gi).
-# so on...
+# Alright, so now we figured out that some cookies have more "potential"
+# or use than others. 
 
+# However, consider the case where we have a cookie cj
+# and a greed gi < gj == cj.
 
+# Now should we assign cj to gj or gi? 
+# In any case, we are still ending up with one match.
+# Therefore, all we should consider is if there is another cookie
+# ci that could satisfy gi. If there is, we could have 2 matches.
+# If there isn't, then it doesn't matter if we assign cj to it.
+
+# Ok, so let's say we're at some gi.
+# If there is ci == gi, then we should assign ci to gi since
+# this is the most optimal cookie (best used here, since can't match higher
+# and not wasted to match lower).
+# If there is not ci == gi, we check ci+1.
+# If gi+1 exists, it doesn't make a difference if we match it with ci+1
+# since we are creating one match in either case.
+# If gi+1 doesn't exist, then this cookie must match downwards, and this
+# cookie would be symmetrical candidates as any of the smaller cookies,
+# creating a match.
 
 # OPTIMAL SOLUTION:
+# Assuming k cookies
+# Assuming n kids/greed
+# Create a hashmap of (ci, num(ci)) for O(1) lookup (O(k))
+# for each children/greed O(n):
+#   all cookies assigned? stop. (keep a counter for optimized termination)
+#   find any match, starting from the optimal to the largest O(k/2) (on average half the cookies will be bigger)
+
 
 # ANALYSIS:
-# O(n)
+# O(nk)
 
 
 
@@ -51,4 +78,20 @@ class Solution(object):
         :type s: List[int]
         :rtype: int
         """
-        
+        if len(g) == 0 or len(s) == 0:
+            return 0
+        g.sort()
+        s.sort()
+        cookiePointer = 0
+        matches = 0
+        for greed in g:
+            if cookiePointer == len(s):
+                return matches
+            while s[cookiePointer] < greed:
+                cookiePointer += 1
+                if cookiePointer == len(s):
+                    return matches
+            cookiePointer += 1
+            matches += 1
+        return matches
+    
