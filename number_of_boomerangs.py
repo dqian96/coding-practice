352b5f81ce63f118d816d84af669969f30d036b5
David Qian <davidqian1996@gmail.com>
2016-11-29 19:59:10 -0500

    why is this so hard

---
+++ number_of_boomerangs.py
@@ -0,0 +1,15 @@
+# Problem: Number of Boomeranges
+# (https://leetcode.com/problems/number-of-boomerangs/)
+
+
+# NOTES:
+# -given n points
+# -each point is 2D i.e. (x, y)
+# -find how many boomerangs there are?
+# -a boomerang is a tuple of points (i, j, k)
+# -such that |i-j| == |i-k|
+# -ORDER matters
+
+
+# CATEGORIZE:
+# search
